01f1b3bf9bc6a238350317946d165bcfb2977ab3
Oliver Krieger <oliver.krieger76@gmail.com>
2024-06-04 15:53:39 +0100

    add script runner

--- script_runner.py
+++ script_runner.py
@@ -1,3 +1,13 @@
 from Scripts.creation.create_module import request_module_create
 
-request_module_create()
+def select_option():
+    print("1 - create new module")
+    selected_option = input("Selected Option:")
+
+    match selected_option:
+        case 1:
+            request_module_create()
+        case _:
+            print("WARNING - No option with selection ", select_option)
+
+select_option()
