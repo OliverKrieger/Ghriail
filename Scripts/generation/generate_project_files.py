aaf166135e9f2ce9020059f398da4ce46b628315
Oliver Krieger <oliver.krieger76@gmail.com>
2024-07-02 22:51:13 +0100

    add way to remove files - WIP on rebuilding and generating files

---
+++ Scripts/generation/generate_project_files.py
@@ -0,0 +1,5 @@
+from Scripts.deletion.remove_compiled import remove_compiled
+
+def regenerate_project_files():
+    remove_compiled()
+    
--- Scripts/utils/paths.py
+++ Scripts/utils/paths.py
@@ -7,3 +7,7 @@ project_name = get_project_name(working_dir)
 unreal_source_dir = os.path.join(working_dir, "Source")
 unreal_uproject_fpath = find_file_with_ext(working_dir, ".uproject")
 unreal_project_buildcs_fpath = os.path.join(unreal_source_dir, project_name, (project_name+".Build.cs"))
+
+unreal_binaries_dir = os.path.join(working_dir, "Binaries")
+unreal_intermediate_dir = os.path.join(working_dir, "Intermediate")
+unreal_sln_fpath = find_file_with_ext(working_dir, ".sln")
