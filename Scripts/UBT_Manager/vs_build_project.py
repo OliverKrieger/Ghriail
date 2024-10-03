cc51fb14b6bc1e327848433d260fb5f694409702
Oliver Krieger <oliver.krieger76@gmail.com>
2024-10-03 14:55:40 +0100

    fix nuget restore via scripts

--- Scripts/UBT_Manager/vs_build_project.py
+++ Scripts/UBT_Manager/vs_build_project.py
@@ -4,6 +4,13 @@ from Scripts.Utils.paths import unreal_sln_path
 from Scripts.Utils.paths_external import MSbuild_fpath
 
 def build_project():
+    nuget_restore_command = [
+        'dotnet',
+        'restore',
+        unreal_sln_path
+    ]
+    subprocess.run(nuget_restore_command, check=True)
+
     msbuild_command = [
         r""+MSbuild_fpath+"",
         unreal_sln_path,
