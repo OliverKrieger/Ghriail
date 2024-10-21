1053a3786bc0810d626331ceceaf2822626e5cc0
Oliver Krieger <oliver.krieger76@gmail.com>
2024-10-21 15:01:12 +0100

    define grid related classes

---
+++ Source/GridModule/Public/CellType.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CellType.generated.h"
+
+UENUM(BlueprintType)
+enum class ECellType : uint8
+{
+    Walkable UMETA(DisplayName = "Walkable"),
+    Air UMETA(DisplayName = "Air"),
+    Impassable UMETA(DisplayName = "Impassable"),
+    DifficultTerrain UMETA(DisplayName = "Difficult Terrain")
+};
---
+++ Source/GridModule/Public/GridCell.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CellType.h"
+
+#include "GridCell.generated.h"
+
+USTRUCT(BlueprintType)
+struct FGridCell
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Test")
+    FVector TestPosition;
+
+    // Example of UPROPERTY if you need it exposed to Unreal
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid")
+    FVector Position;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid")
+    float Cost;
+
+    // Enumeration for cell types (walkable, impassable, etc.)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid")
+    ECellType Type;
+
+    FGridCell() {
+        Position = FVector(.0f, .0f, .0f);
+        Cost = 1;
+        Type = ECellType::Impassable;
+    }
+};
